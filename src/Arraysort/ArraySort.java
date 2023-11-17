de3ba1e73d5df0a4f7f1cc827ee425504e63ec82
realdjun2 <realdjunodon@gmail.com>
2023-11-17 17:55:02 +0000

    Added ArraySort Package

---
+++ src/Arraysort/ArraySort.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class ArraySort{
+    public static void main(String[]args){
+        int[] array = {9,7,8,15,3,2};
+        
+        Arrays.sort(array);
+        System.out.println("Sorted Array");
+        for(int value: array){
+            System.out.println(value+"");
+        }
+      }
+}
