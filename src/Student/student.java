b869bfab57fcca4284053709832719eec898f4fc
realdjun2 <realdjunodon@gmail.com>
2023-11-23 21:51:12 +0000

    Added Student Package

---
+++ src/Student/student.java
@@ -0,0 +1,20 @@
+package Student;
+
+public class student {
+    
+    
+    public static void main(String[] args) {
+       Realdjun put = new Realdjun();
+       
+       System.out.println("Enter your Name: ");
+       put.setName();
+       System.out.println("your Section: ");
+       put.setSection();
+       System.out.println("your Motto: ");
+       put.setMotto();
+       System.out.println("\nSTUDENT'S PROFILE!\n");
+       put.Info();
+        
+    }
+    
+}    
